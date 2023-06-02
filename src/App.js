6808b423c452686f11a4e4ef3fa6e15c2e8e1833
Krishn-Thakrar <84504018+Krishn-Thakrar@users.noreply.github.com>
2023-06-02 12:32:43 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import React from "react";
 import Login from "./pages/Login";
 import ProductList from "./pages/ProductList";
@@ -8,27 +7,32 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { AuthWrapper } from "./context/auth";
+import SearchBar from "./components/SearchBar";
 
 function App(){
+  const Backward = <Navigate to={"/login"} />;
+  const Forward = <Navigate to={"/productlist"} />;
+  const x = JSON.parse(localStorage.getItem("user"));
+  console.log(x.id);
+
   return(
     <>
-    <div className="main">
-      <div className="headmain">
-        <Header />
-      </div>
-      <div className="body">
-        <ToastContainer />
+      <div className="main">
         <BrowserRouter>
-          <Routes>
-            <Route path="/register" Component={Register}></Route>
-            <Route path="/product-list" Component={ProductList}></Route>
-            <Route path="/login" Component={Login}></Route>
-          </Routes>
+          <AuthWrapper>
+            <ToastContainer />
+            <Header />
+            <SearchBar />
+            <Routes>
+              <Route path="/" Component={Register}></Route>
+              <Route path="/register" Component={Register}></Route>
+              <Route path="/productlist" element={x.id ? Forward : Backward}></Route>
+              <Route path="/login" Component={Login}></Route>
+            </Routes>
+            <Footer />
+          </AuthWrapper>
         </BrowserRouter>
-      </div>
-      <div className="footmain">
-        <Footer />
-      </div>
       </div>
     </>
   );

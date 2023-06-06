f046f39e5ebe7cc0986a325a4a6e5ec368d7b688
Krishn-Thakrar <84504018+Krishn-Thakrar@users.noreply.github.com>
2023-06-06 12:03:52 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import Login from "./pages/Login";
 import ProductList from "./pages/ProductList";
@@ -9,13 +9,16 @@ import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthWrapper } from "./context/auth";
 import SearchBar from "./components/SearchBar";
+import Book from "./pages/Book";
+import AddBook from "./pages/AddBook"
+import Categories from "./pages/Categories";
+import AddCategories from "./pages/AddCategories";
+import User from "./pages/User";
+import EditUser from "./pages/EditUser";
+import CartPage from "./pages/CartPage";
+import UpdateProfile from "./pages/UpdateProfile";
 
 function App(){
-  const Backward = <Navigate to={"/login"} />;
-  const Forward = <Navigate to={"/productlist"} />;
-  const x = JSON.parse(localStorage.getItem("user"));
-  console.log(x.id);
-
   return(
     <>
       <div className="main">
@@ -25,10 +28,20 @@ function App(){
             <Header />
             <SearchBar />
             <Routes>
-              <Route path="/" Component={Register}></Route>
+              <Route path="/" Component={Login}></Route>
               <Route path="/register" Component={Register}></Route>
-              <Route path="/productlist" element={x.id ? Forward : Backward}></Route>
+              <Route path="/productlist" Component={ProductList}></Route>
               <Route path="/login" Component={Login}></Route>
+              <Route path="/book" Component={Book}></Route>
+              <Route path="/add-book" Component={AddBook}></Route>
+              <Route path="/add-book/:id" Component={AddBook}></Route>
+              <Route path="/categories" Component={Categories}></Route>
+              <Route path="/add-category" Component={AddCategories}></Route>
+              <Route path="/add-category/:id" Component={AddCategories}></Route>
+              <Route path="/user" Component={User}></Route>
+              <Route path="/edit-user/:id" Component={EditUser}></Route>
+              <Route path="/cart-page" Component={CartPage}></Route>
+              <Route path="/update-profile" Component={UpdateProfile}></Route>
             </Routes>
             <Footer />
           </AuthWrapper>
